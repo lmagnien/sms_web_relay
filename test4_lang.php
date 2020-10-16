7648f18b8e6f400abe2ce31d0cb78c1de15c123a
Laurent Magnien <lmagnien+github@gmail.com>
2020-10-16 23:06:00 +0200

    Delete test4_lang.php

--- test4_lang.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-// Affiche le nom d'utilisateur qui fait tourner le processus php/http
-// (sur un système ayant "whoami" dans le chemin d'exécutables)
-$a = file_get_contents('index.php');
-var_dump($a);
-//echo exec('whoami');
-?>
