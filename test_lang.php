a2d74d5e130d6206663e2a64e44597e9b5c2a932
Laurent Magnien <lmagnien+github@gmail.com>
2020-10-16 22:53:09 +0200

    Update test_lang.php

--- test_lang.php
+++ test_lang.php
@@ -2,5 +2,6 @@
 <?php
 // Affiche le nom d'utilisateur qui fait tourner le processus php/http
 // (sur un système ayant "whoami" dans le chemin d'exécutables)
-echo exec('whoami');
+file_get_contents('index.php');
+//echo exec('whoami');
 ?>
